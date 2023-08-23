fc588b2452e6aa2e1f628345e6259095d4434f99
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-23 15:40:50 +0330

    update app.jsx and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import {
+  About,
+  Appointment,
+  Banner,
+  Benefits,
+  Features,
+  Footer,
+  Header,
+  Latest,
+  Services,
+  Testimonials,
+} from "./components";
+
 import styles from "./styles";
 const App = () => (
   <div className="w-full bg-white overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter} `}>
-      <div className={` ${styles.boxWidth}`}>Header</div>
+      <div className={` ${styles.boxWidth}`}>
+        <Header />
+      </div>
     </div>
     <div className={`${styles.flexStart} `}>
-      <div className={` ${styles.boxWidth}`}>Banner</div>
+      <div className={` ${styles.boxWidth}`}>
+        <Banner />
+      </div>
     </div>
 
     <div className={`${styles.flexStart} `}>
       <div className={` ${styles.boxWidth}`}>
-        Features About Services Testimonials Benefits Appointment Latest Footer
+        <Features />
+        <About />
+        <Services />
+        <Testimonials />
+        <Benefits />
+        <Appointment />
+        <Latest />
+        <Footer />
       </div>
     </div>
   </div>
---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import About from "./About";
+import Appointment from "./Appointment";
+import Banner from "./Banner";
+import Benefits from "./Benefits";
+import Features from "./Features";
+import Footer from "./Footer";
+import Header from "./Header";
+import Latest from "./Latest";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
+export {
+  About,
+  Appointment,
+  Banner,
+  Benefits,
+  Features,
+  Footer,
+  Header,
+  Latest,
+  Services,
+  Testimonials,
+};

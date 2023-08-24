518a845bb400a683249690039f36c7f9772da809
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-24 14:09:25 +0330

    Add pics of the Banner component

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,22 @@ import linkedIn from "./linkedIn.svg";
 import reservation from "./reservation.svg";
 import close from "./close.svg";
 import menu from "./menu.svg";
+import welcome from "./welcome.svg";
+import bg1 from "./bg1.png";
+import bg2 from "./bg2.png";
+import flowergirl from "./flowergirl.png";
 
-export { Logo, call, facebook, twitter, linkedIn, reservation, close, menu };
+export {
+  Logo,
+  call,
+  facebook,
+  twitter,
+  linkedIn,
+  reservation,
+  close,
+  menu,
+  welcome,
+  bg1,
+  bg2,
+  flowergirl,
+};

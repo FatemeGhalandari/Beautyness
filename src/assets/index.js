92507ce134429a333497f711a5074c37a3d5ee3d
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-25 15:43:32 +0330

    Add pic files and Update index.js to export them

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,7 +9,29 @@ import menu from "./menu.svg";
 import welcome from "./welcome.svg";
 import bg1 from "./bg1.png";
 import bg2 from "./bg2.png";
-import flowergirl from "./flowergirl.png";
+import bannerGirl from "./bannerGirl.png";
+import aboutGirl from "./aboutGirl.png";
+import feature1 from "./feature1.png";
+import feature2 from "./feature2.png";
+import feature3 from "./feature3.png";
+import about1 from "./about1.svg";
+import about2 from "./about2.svg";
+import about3 from "./about3.svg";
+import quote from "./quote.svg";
+import star_half from "./star_half.svg";
+import star from "./star.svg";
+import testimonial1 from "./testimonial1.png";
+import testimonial2 from "./testimonial2.png";
+import dots from "./dots.svg";
+import email from "./email.svg";
+import name from "./name.svg";
+import note from "./note.svg";
+import phone from "./phone.svg";
+import service from "./service.svg";
+import tick from "./tick.svg";
+import latest1 from "./latest1.svg";
+import latest2 from "./latest2.svg";
+import latest3 from "./latest3.svg";
 
 export {
   Logo,
@@ -23,5 +45,27 @@ export {
   welcome,
   bg1,
   bg2,
-  flowergirl,
+  bannerGirl,
+  feature1,
+  feature2,
+  feature3,
+  about1,
+  about2,
+  about3,
+  aboutGirl,
+  quote,
+  testimonial1,
+  testimonial2,
+  star,
+  star_half,
+  dots,
+  email,
+  name,
+  tick,
+  note,
+  phone,
+  service,
+  latest1,
+  latest2,
+  latest3,
 };

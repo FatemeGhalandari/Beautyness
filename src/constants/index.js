2621114e2c84f5f5bdd09d298d1519d7215d07a2
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-26 15:01:45 +0330

    Add benefits and offers array

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { email, name, note, phone, service } from "../assets";
+
 export const headerLinks = [
   {
     id: "home",
@@ -18,5 +20,70 @@ export const headerLinks = [
   {
     id: "contact",
     title: "Contact",
+  },
+];
+export const benefits = [
+  {
+    id: "benefit1",
+    title: "Get Expert Advice",
+    content:
+      "Your skin needs regular care to look flawless and beautiful. At the same time, in some cases, you need special treatments to fight against exceptional skin conditions. Only a trained and experienced beauty expert can understand what your skin needs most and how to offer the same. Hence, you should visit the salon for expert advice to look gorgeous and have flawless skin.",
+  },
+  {
+    id: "benefit2",
+    title: "Complete Beauty Service",
+    content:
+      "From hair to toe – a beauty salon can offer you complete treatment and care for your body. You can trim and style your hair, clean your skin, rejuvenate your face, take care of your hands and feet and obtain many other services from a beauty salon necessary to maintain your appearance and hygiene.",
+  },
+  {
+    id: "benefit3",
+    title: "Know What You Need",
+    content:
+      "Your beauty expert will suggest the right kind of skincare routine or therapies that you must have. After talking to them, you can easily understand what you should offer to your oily skin or how to manage your curly hair. This is a great help to keep yourself in presentable condition all the time.",
+  },
+  {
+    id: "benefit4",
+    title: "Forget Stress",
+    content:
+      "One hour in a beauty salon can help you to feel special. Those comfortable chairs, soft massaging hands, excellent products, and that fabulous ambience – everything will make you feel special during the service. You can forget all your stress and feel relaxed during your sessions with your beauty experts.",
+  },
+  {
+    id: "benefit5",
+    title: "Personal Care",
+    content:
+      "Let’s face it: we can’t spend every single day at a salon. However, when we do go, the professionals can guide us regarding how to care for our hair and eyebrows, pluck our eyebrows, and even change our hair color.",
+  },
+  {
+    id: "benefit6",
+    title: "Get Advice And Tips",
+    content:
+      "Your hairstylist will help you find the best haircut for your face shape and share some tricks and special treatments to strengthen your hair, eyebrows, and eyelashes. Although the final decision will always be up to you, this advice will come in handy when it comes to deciding to change your look.",
+  },
+];
+export const offers = [
+  {
+    id: "name",
+    title: "Name",
+    content: name,
+  },
+  {
+    id: "email",
+    title: "Email",
+    content: email,
+  },
+  {
+    id: "phone",
+    title: "Phone",
+    content: phone,
+  },
+  {
+    id: "service",
+    title: "Service",
+    content: service,
+  },
+  {
+    id: "note",
+    title: "Any Note For Us",
+    content: note,
   },
 ];

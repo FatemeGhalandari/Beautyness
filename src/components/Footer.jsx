f99f43af42591db9f3eaaa9fc6b6fa107151686a
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-27 16:49:11 +0330

    Update Footer to make it responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,12 @@ import { footerLinks } from "../constants";
 const Footer = () => (
   <div className="flex flex-col ">
     <div className="flex flex-col bg-[#1E2C30] divide-y-2 divide-[#5C8692] md:px-32 px-8">
-      <div className="flex md:flex-row flex-col py-8 px-24 ">
-        <div className="flex justify-start items-start ">
+      <div className="flex md:flex-row flex-col py-8 sm:px-24 px-4">
+        <div className="flex flex-1 justify-center py-4">
           <img
             src={Logo_white}
             alt="Logo_white"
-            className="w-[298px] h-[65px] leading-[125%]"
+            className="w-[298px] h-[65px] object-contain"
           />
         </div>
         <div className="flex md:justify-end justify-center items-center md:ml-auto ml-2 md:gap-2 gap-8">
@@ -43,20 +43,20 @@ const Footer = () => (
           />
         </div>
       </div>
-      <div className="flex flex-row py-8 sm:px-24 px-12 mx-3 flex-[1.5] w-full justify-between flex-wrap xlg:mt-0 md:mt-8 mt-10">
+      <div className="flex flex-row sm:py-8 py-2 sm:px-24 px-12 mx-3 w-full justify-between flex-wrap ">
         {footerLinks.map((footerLink) => (
           <div
             key={footerLink.links.id}
-            className="flex flex-col ss:my-0 my-4 min-w-[150px] max-w-[324px]"
+            className="flex flex-col ss:my-4 my-2 min-w-[150px] max-w-[324px]"
           >
             <h4 key={footerLink.links.id} className={`${styles.footers}`}>
               {footerLink.title}
             </h4>
-            <ul className="list-none mt-4" key={footerLink.links.id}>
+            <ul className="list-none sm:mt-4 mt-2" key={footerLink.links.id}>
               {footerLink.links.map((link, index) => (
                 <li
                   key={link.id}
-                  className={`text-[#FFF] font-manrope text-[15px] font-normal leading-[165%] max-w-[327px] cursor-pointer ${
+                  className={`text-[#EDF5F7] font-manrope text-[15px] font-normal leading-[165%] max-w-[327px] cursor-pointer ${
                     index !== footerLink.links.length - 1 ? "mb-2" : "mb-0"
                   }`}
                 >
@@ -69,7 +69,7 @@ const Footer = () => (
       </div>
     </div>
     <div className="flex items-center justify-center bg-[#111E22] py-2">
-      <p className="text-[#FFF] font-manrope text-[18px] font-normal leading-[200%] py-1">
+      <p className="text-[#FFF] font-manrope sm:text-[18px] text-center text-[15px] font-normal leading-[200%] py-1 px-3">
         © Copyright 2023 Beautyness. All Rights Reserved.
       </p>
     </div>

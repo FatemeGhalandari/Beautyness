229b69afc4aaf3db40f0066579fddf6c17dba485
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-27 15:51:15 +0330

    Update Testimonials component to add the texts and pics

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,145 @@
-import React from 'react'
+import styles from "../styles";
+import { quote, star, star_half, testimonial1, testimonial2 } from "../assets";
 
-const Testimonials = () => {
-  return (
-    <div>
-      
+const Testimonials = () => (
+  <div className="flex flex-col bg-[#EDF5F7] py-24 ">
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-[#5C8692] font-manrope sm:text-[15px] text-[13px] font-normal leading-[154%] max-w-[327px] uppercase tracking-wider">
+          Testimonials
+        </p>
+        <h2 className="text-[#141414] font-playfair sm:text-[45px] text-[30px] text-center mx-4 font-bold leading-[130%] mt-4 tracking-tight">
+          What our Customers says...
+        </h2>
+      </div>
+      <div className="flex md:flex-row flex-col py-12 mx-4 md:items-start items-center justify-center gap-8 ">
+        <div className="flex flex-col items-center justify-center bg-[#FFF] rounded-t-[6px]">
+          <p className="font-manrope text-[#141414] max-w-[582px] sm:text-[19px] text-[17px] font-normal leading-[150%] sm:px-24 px-12 py-8">
+            "I've been a loyal customer of Beautyness for over two years now,
+            and I can confidently say that it's the best beauty salon I've ever
+            been to. The staff is incredibly skilled and friendly, always making
+            me feel welcome and pampered. Whether I'm getting a facial, a
+            haircut, or a manicure, I leave the salon feeling rejuvenated and
+            confident. Beautyness has truly become my go-to place for all things
+            beauty!"
+          </p>
+          <div className="bg-[#1E2C30] flex flex-row w-full px-12 py-6 rounded-b-[6px]">
+            <div className="flex sm:flex-row flex-col items-start justify-start ">
+              <img
+                src={testimonial1}
+                alt="testimonial1"
+                className="w-[74px] h-[74px] border-solid border-2 mr-4"
+              />
+              <div className="flex flex-col sm:mt-0 mt-2">
+                <div className="flex flex-row">
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star_half}
+                    alt="star_half"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                </div>
+                <h2 className="text-[#fff] font-playfair sm:text-[20px] text-[18px] font-bold leading-[160%] tracking-tight">
+                  James Williams
+                </h2>
+                <p className="text-[#fff] font-manrope text-[14px] font-normal leading-[160%] tracking-normal">
+                  United States
+                </p>
+              </div>
+            </div>
+            <div className="flex ml-auto py-2">
+              <img
+                src={quote}
+                alt="quotes"
+                className="w-[70.012px] h-[55.273px]"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center bg-white rounded-t-[6px]">
+          <p className="font-manrope text-[#141414] max-w-[582px] sm:text-[19px] text-[17px] font-normal leading-[150%] sm:px-24 px-12 py-8  ">
+            "Beautyness is my little haven of relaxation and transformation.
+            Walking into the salon instantly puts me in a calm state of mind,
+            and the services they offer take that experience to the next level.
+            From their luxurious spa treatments to their precision hair styling,
+            each visit is a delightful escape from the hustle and bustle of
+            daily life. The attention to detail and dedication to customer
+            satisfaction make Beautyness a standout in the beauty industry."
+          </p>
+          <div className="bg-[#1E2C30] flex flex-row w-full px-12 py-6 rounded-b-[6px]">
+            <div className="flex sm:flex-row flex-col items-start justify-start ">
+              <img
+                src={testimonial2}
+                alt="testimonial2"
+                className="w-[74px] h-[74px] border-solid border-2 mr-4"
+              />
+              <div className="flex flex-col sm:mt-0 mt-2">
+                <div className="flex flex-row">
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star}
+                    alt="star"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                  <img
+                    src={star_half}
+                    alt="star_half"
+                    className="w-[16.382px] h-[16.2px]"
+                  />
+                </div>
+                <h2 className="text-[#fff] font-playfair sm:text-[20px] text-[18px] font-bold leading-[160%] tracking-tight">
+                  Lieo Jessica
+                </h2>
+                <p className="text-[#fff] font-manrope text-[14px] font-normal leading-[160%] tracking-normal">
+                  United Kingdom
+                </p>
+              </div>
+            </div>
+            <div className="flex ml-auto py-2">
+              <img
+                src={quote}
+                alt="quotes"
+                className="w-[70.012px] h-[55.273px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Testimonials
+export default Testimonials;

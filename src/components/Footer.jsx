9e447fcb1d696d15f40cb957926b00a4315e34ae
Fateme Ghalandari <fghalandarii@gmail.com>
2023-08-28 20:47:42 +0330

    Fix position and add paddings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,15 +12,15 @@ import { footerLinks } from "../constants";
 const Footer = () => (
   <div className="flex flex-col ">
     <div className="flex flex-col bg-[#1E2C30] divide-y-2 divide-[#5C8692] md:px-32 px-8">
-      <div className="flex md:flex-row flex-col py-8 sm:px-24 px-4">
-        <div className="flex flex-1 justify-center py-4">
+      <div className="flex sm:flex-row flex-col py-8 sm:px-24 px-4">
+        <div className="flex flex-1 md:justify-start md:items-start justify-center py-4">
           <img
             src={Logo_white}
             alt="Logo_white"
             className="w-[298px] h-[65px] object-contain"
           />
         </div>
-        <div className="flex md:justify-end justify-center items-center md:ml-auto ml-2 md:gap-2 gap-8">
+        <div className="flex md:justify-end justify-center items-center md:ml-auto ml-2 md:gap-2 sm:gap-4 gap-8">
           <FontAwesomeIcon
             icon={faFacebookF}
             style={{ color: "#FFF" }}
@@ -43,21 +43,21 @@ const Footer = () => (
           />
         </div>
       </div>
-      <div className="flex flex-row sm:py-8 py-2 sm:px-24 px-12 mx-3 w-full justify-between flex-wrap ">
-        {footerLinks.map((footerLink) => (
+      <div className="flex flex-row sm:py-8 py-2 sm:px-24 px-12 mx-3 w-full justify-between sm:items-start items-center flex-wrap ">
+        {footerLinks.map((footerLink, index) => (
           <div
-            key={footerLink.links.id}
+            key={index}
             className="flex flex-col ss:my-4 my-2 min-w-[150px] max-w-[324px]"
           >
-            <h4 key={footerLink.links.id} className={`${styles.footers}`}>
+            <h4 key={index} className={`${styles.footers}`}>
               {footerLink.title}
             </h4>
-            <ul className="list-none sm:mt-4 mt-2" key={footerLink.links.id}>
-              {footerLink.links.map((link, index) => (
+            <ul key={index} className="list-none sm:mt-4 mt-2">
+              {footerLink.links.map((link, index2) => (
                 <li
-                  key={link.id}
-                  className={`text-[#EDF5F7] font-manrope text-[15px] font-normal leading-[165%] max-w-[327px] cursor-pointer ${
-                    index !== footerLink.links.length - 1 ? "mb-2" : "mb-0"
+                  key={index2}
+                  className={`text-[#EDF5F7] font-manrope text-[15px] font-normal leading-[165%] max-w-[200px] cursor-pointer ${
+                    index2 !== footerLink.links.length - 1 ? "mb-2" : "mb-0"
                   }`}
                 >
                   {link.name}

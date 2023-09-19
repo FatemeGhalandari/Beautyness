4db8cf90191899b559e878fb0d3057336c830fd0
Fateme Ghalandari <fghalandarii@gmail.com>
2023-09-20 02:32:32 +0330

    Add components of the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Banner from "../components/About/Banner";
+import Features from "../components/About/Features";
+import AboutUs from "../components/About/AboutUs";
+import styles from "../styles";
 
-const About = () => {
-  return (
-    <div>
-      
+const About = () => (
+  <div className="w-full bg-white overflow-hidden">
+    <div className={`${styles.flexStart} `}>
+      <div className={` ${styles.boxWidth}`}>
+        <Banner />
+        <Features />
+        <AboutUs />
+      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default About
+export default About;

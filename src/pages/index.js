e82135f0eadb14457979b35b9828c085172f8027
Fateme Ghalandari <fghalandarii@gmail.com>
2023-09-20 02:36:22 +0330

    Import and export all the pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import About from "./About";
+import Blog from "./Blog";
+import Blogs from "./Blogs";
+import Contact from "./Contact";
+import Notfound from "./Notfound";
+import Reservation from "./Reservation";
+import Services from "./Services";
+
+export { Home, About, Blog, Blogs, Contact, Notfound, Reservation, Services };

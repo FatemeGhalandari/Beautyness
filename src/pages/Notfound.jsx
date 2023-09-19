9e4db04ac74a5d63ef71f53644dc21ffb489129e
Fateme Ghalandari <fghalandarii@gmail.com>
2023-09-19 23:41:06 +0330

    Initialize pages

---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Notfound
---
+++ src/pages/Reservation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reservation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Reservation

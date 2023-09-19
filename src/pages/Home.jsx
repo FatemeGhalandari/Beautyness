970ac02743133bc4d78870029f23fa4822de1b37
Fateme Ghalandari <fghalandarii@gmail.com>
2023-09-19 23:39:19 +0330

    Home page component (previous App component)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import {
+  About,
+  Appointment,
+  Banner,
+  Benefits,
+  Features,
+  Footer,
+  Header,
+  Latest,
+  Testimonials,
+} from "../components";
+
+import styles from "../styles";
+
+const Home = () => (
+  <div className="w-full bg-white overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+      <div className={` ${styles.boxWidth}`}>
+        <Header />
+      </div>
+    </div>
+    <div className={`${styles.flexStart} `}>
+      <div className={` ${styles.boxWidth}`}>
+        <Banner />
+      </div>
+    </div>
+
+    <div className={`${styles.flexStart} `}>
+      <div className={` ${styles.boxWidth}`}>
+        <Features />
+        <About />
+        <Benefits />
+        <Testimonials />
+        <Latest />
+        <Appointment />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;

cf0e4d4d82a60425e639ccb1df116c21f0885893
Fateme Ghalandari <fghalandarii@gmail.com>
2023-09-20 02:37:23 +0330

    add About page/about component image

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,6 +17,7 @@ import feature3 from "./feature3.png";
 import about1 from "./about1.svg";
 import about2 from "./about2.svg";
 import about3 from "./about3.svg";
+import about4 from "./about4.png";
 import quote from "./quote.svg";
 import star_half from "./star_half.svg";
 import star from "./star.svg";
@@ -56,6 +57,7 @@ export {
   about1,
   about2,
   about3,
+  about4,
   aboutGirl,
   quote,
   testimonial1,

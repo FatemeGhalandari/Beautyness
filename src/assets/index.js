45a028e3f8679b15458befa69c28f5aba9f37ee4
Fateme Ghalandari <fghalandarii@gmail.com>
2023-09-24 19:33:57 +0330

    Add Services page images

--- src/assets/index.js
+++ src/assets/index.js
@@ -42,6 +42,11 @@ import expert3 from "./expert3.png";
 import share1 from "./share1.png";
 import share2 from "./share2.png";
 import share3 from "./share3.png";
+import service1 from "./service1.jpg";
+import service2 from "./service2.jpg";
+import service3 from "./service3.jpg";
+import service4 from "./service4.jpg";
+import service5 from "./service5.jpg";
 
 export {
   Logo,
@@ -88,4 +93,9 @@ export {
   share1,
   share2,
   share3,
+  service1,
+  service2,
+  service3,
+  service4,
+  service5,
 };

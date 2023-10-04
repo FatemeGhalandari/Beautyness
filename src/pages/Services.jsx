3325fb795a6851f117bc9f1442d20446f74da57a
Fateme Ghalandari <fghalandarii@gmail.com>
2023-10-05 02:45:42 +0330

    Add appointment component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,13 +1,14 @@
 import styles from "../styles";
 import Banner from "../components/ServicesPage/Banner";
 import Service from "../components/ServicesPage/Service";
-
+import Appointment from '../components/Appointment'
 const Services = () => (
   <div className="w-full bg-white overflow-hidden">
     <div className={`${styles.flexStart} `}>
       <div className={` ${styles.boxWidth}`}>
         <Banner />
         <Service />
+        <Appointment />
       </div>
     </div>
   </div>

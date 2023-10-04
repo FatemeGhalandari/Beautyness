21cc6285d81efa809de786b1c36261e7ed740f02
Fateme Ghalandari <fghalandarii@gmail.com>
2023-10-04 18:44:39 +0330

    Update to make data dynamic

--- src/components/ServicesPage/ServiceCard.jsx
+++ src/components/ServicesPage/ServiceCard.jsx
@@ -1,53 +1,38 @@
 import styles from "../../styles";
-import { service1 } from "../../assets";
+import { services } from "../../data";
 
 const ServiceCard = () => (
-  <div className="flex flex-row py-12 items-start justify-center">
-    <img
-      src={service1}
-      alt="service1"
-      className="w-[600px] h-[640px] rounded-sm"
-    />
-    <div className="flex flex-col gap-4 py-6 px-8 ">
-      <div className="flex flex-col gap-2 border-[#9BB7BF] border-b py-2">
-        <p className={styles.serviceItem}>
-          Full body massage ............................. $54.00
-        </p>
-        <p className={styles.serviceDesc}>
-          The versions have evolved over the sometimes by accident.
-        </p>
+  <div className="flex flex-col py-12 items-start justify-center gap-8 ">
+    {services.map((service) => (
+      <div
+        key={service.id}
+        className="flex flex-row border-b-2 border-[#5C8692] py-8"
+      >
+        <img
+          src={service.avatar}
+          alt={service.id}
+          className="w-[500px] h-[540px] rounded-sm"
+        />
+        <div className="flex flex-col gap-4 py-2 px-8 ">
+          {service.experiences.map((experience) => (
+            <div
+              key={experience.id}
+              className="flex flex-col gap-2 border-[#9BB7BF] border-b py-2"
+            >
+              <p className={styles.serviceItem}>{experience.item}</p>
+              <p className={styles.serviceDesc}>{experience.desc}</p>
+            </div>
+          ))}
+
+          <div className="flex py-8">
+            <button type="button" className={`${styles.bt}`}>
+              <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
+              <span className={`${styles.btSpan}`}>get an appointment</span>
+            </button>
+          </div>
+        </div>{" "}
       </div>
-      <div className="flex flex-col gap-2 border-[#9BB7BF] border-b py-2">
-        <p className={styles.serviceItem}>
-          Geothermal spa ................................. $54.00
-        </p>
-        <p className={styles.serviceDesc}>
-          The versions have evolved over the sometimes by accident.
-        </p>
-      </div>
-      <div className="flex flex-col gap-2 border-[#9BB7BF] border-b py-2">
-        <p className={styles.serviceItem}>
-          Sauna relax ....................................... $73.00
-        </p>
-        <p className={styles.serviceDesc}>
-          The versions have evolved over the sometimes by accident.
-        </p>
-      </div>
-      <div className="flex flex-col gap-3">
-        <p className={styles.serviceItem}>
-          Face masks ....................................... $48.00
-        </p>
-        <p className={styles.serviceDesc}>
-          The versions have evolved over the sometimes by accident.
-        </p>
-      </div>
-      <div className="flex py-8">
-        <button type="button" className={`${styles.bt}`}>
-          <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
-          <span className={`${styles.btSpan}`}>get an appointment</span>
-        </button>
-      </div>
-    </div>
+    ))}
   </div>
 );
 

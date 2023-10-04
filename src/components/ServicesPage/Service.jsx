31bedc1d0988bc149f9373cf71cf1cca6d8d4b34
Fateme Ghalandari <fghalandarii@gmail.com>
2023-10-05 02:46:44 +0330

    Update to make responsive

--- src/components/ServicesPage/Service.jsx
+++ src/components/ServicesPage/Service.jsx
@@ -2,7 +2,7 @@ import ServiceCard from "./ServiceCard";
 
 const Service = () => (
   <div className="flex flex-col py-20 items-center">
-    <div className="flex flex-col">
+    <div className="flex flex-col px-8">
       <p className="text-[#5C8692] text-center font-manrope text-[15px] font-normal leading-[154.5%] tracking-wide uppercase">
         Our Services
       </p>
--- src/components/ServicesPage/ServiceCard.jsx
+++ src/components/ServicesPage/ServiceCard.jsx
@@ -2,18 +2,20 @@ import styles from "../../styles";
 import { services } from "../../data";
 
 const ServiceCard = () => (
-  <div className="flex flex-col py-12 items-start justify-center gap-8 ">
-    {services.map((service) => (
+  <div className="flex flex-col py-12 items-center justify-center sm:gap-8 gap-2">
+    {services.map((service, index) => (
       <div
         key={service.id}
-        className="flex flex-row border-b-2 border-[#5C8692] py-8"
+        className={`flex md:flex-row flex-col py-8 border-[#5C8692] px-2 ${
+          index !== services.length - 1 ? "border-b-2" : "border-b-0"
+        }`}
       >
         <img
           src={service.avatar}
           alt={service.id}
-          className="w-[500px] h-[540px] rounded-sm"
+          className="md:w-[500px] md:h-[540px] object-cover rounded-sm px-4"
         />
-        <div className="flex flex-col gap-4 py-2 px-8 ">
+        <div className="flex flex-col gap-4 py-2 pl-4">
           {service.experiences.map((experience) => (
             <div
               key={experience.id}

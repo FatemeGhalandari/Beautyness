b1ba43ab398d9f1ce122eb68f6497a9774fcf4ed
unknown <fghalandarii@gmail.com>
2023-10-17 19:12:52 +0330

    Add components

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
-const Reservation = () => {
-  return (
-    <div>
-      
+import Banner from "../components/ServicePage/Banner";
+import About from "../components/ServicePage/About";
+import Appointment from "../components/ServicePage/Appointment";
+import Membership from "../components/ServicePage/Membership";
+import Benefits from "../components/Benefits";
+import styles from "../styles";
+const Reservation = () => (
+  <div className="w-full bg-white overflow-hidden">
+    <div className={`${styles.flexStart} `}>
+      <div className={` ${styles.boxWidth}`}>
+        <Banner />
+        <About />
+        <Membership />
+        <Appointment />
+        <Benefits />
+      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Reservation
+export default Reservation;

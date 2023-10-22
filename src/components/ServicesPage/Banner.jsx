a0008104e392ae1bec947cdc1fd677b1f19d61a3
unknown <fghalandarii@gmail.com>
2023-10-22 19:43:37 +0330

    Fix NavLink

--- src/components/ServicesPage/Banner.jsx
+++ src/components/ServicesPage/Banner.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 const Banner = () => {
   return (
     <div className="bg-[#1E2C30] w-full sm:relative">
@@ -8,15 +9,17 @@ const Banner = () => {
         <h4 className="text-[#FFF] text-center font-playfair sm:text-[50px] text-[40px] font-semibold leading-[120%] tracking-tight max-w-[650px]">
           Well Experience Beauty at your Service
         </h4>
-        <button
-          type="button"
-          className="flex justify-center items-start py-4 sm:px-8 px-4 bg-[#7A9CA5] sm:mt-8 mt-4 rounded-[4px] group overflow-hidden relative"
-        >
-          <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
-          <span className="text-[#FFF] text-center font-Manrope sm:text-[14px] text-[12px] font-normal tracking-widest leading-[125%] uppercase relative">
-            Make a reservation
-          </span>
-        </button>
+        <NavLink to="/Reservation">
+          <button
+            type="button"
+            className="flex justify-center items-start py-4 sm:px-8 px-4 bg-[#7A9CA5] sm:mt-8 mt-4 rounded-[4px] group overflow-hidden relative"
+          >
+            <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
+            <span className="text-[#FFF] text-center font-Manrope sm:text-[14px] text-[12px] font-normal tracking-widest leading-[125%] uppercase relative">
+              Make a reservation
+            </span>
+          </button>
+        </NavLink>
       </div>
     </div>
   );

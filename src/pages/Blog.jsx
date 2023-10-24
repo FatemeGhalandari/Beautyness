91cd9d189b88f203ea4c5dccc780b6508809c5f7
unknown <fghalandarii@gmail.com>
2023-10-24 17:57:40 +0330

    Add blog component

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
-const Blog = () => {
-  return (
-    <div>
-      
+import Banner from "../components/Blog/Banner";
+import Story from "../components/Blog/Story";
+import styles from "../styles";
+const Reservation = () => (
+  <div className="w-full bg-white overflow-hidden">
+    <div className={`${styles.flexStart} `}>
+      <div className={` ${styles.boxWidth}`}>
+        <Banner />
+        <Story />
+      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Blog
+export default Reservation;

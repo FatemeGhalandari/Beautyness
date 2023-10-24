3109a3f30e1fb6f5575878e755522be0b67131ac
unknown <fghalandarii@gmail.com>
2023-10-24 18:45:42 +0330

    Update to change folder name

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles";
-import Banner from "../components/ServicesPage/Banner";
-import Service from "../components/ServicesPage/Service";
+import Banner from "../components/Services/Banner";
+import Service from "../components/Services/Service";
 import Appointment from '../components/Appointment'
 const Services = () => (
   <div className="w-full bg-white overflow-hidden">

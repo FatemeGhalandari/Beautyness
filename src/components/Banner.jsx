eb85bf3668fb07c1c6c2967d29d9288823923905
unknown <fghalandarii@gmail.com>
2023-10-24 18:42:59 +0330

    Update to Add NavLink

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { bannerGirl, welcome } from "../assets";
 import styles from "../styles";
 
@@ -24,15 +25,17 @@ const Banner = () => (
         our top beauty treatment list of 2022 is here to ensure that you
         definitely won’t be short for options.
       </p>
-      <button
-        type="button"
-        className="flex justify-center items-start py-4 sm:px-8 px-4 bg-[#7A9CA5] sm:mt-8 mt-4 rounded-[4px] group overflow-hidden relative"
-      >
-        <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
-        <span className="text-[#FFF] text-center font-Manrope sm:text-[14px] text-[12px] font-normal tracking-widest leading-[125%] uppercase relative">
-          Make a reservation
-        </span>
-      </button>
+      <NavLink to="/Reservation">
+        <button
+          type="button"
+          className="flex justify-center items-start py-4 sm:px-8 px-4 bg-[#7A9CA5] sm:mt-8 mt-4 rounded-[4px] group overflow-hidden relative"
+        >
+          <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
+          <span className="text-[#FFF] text-center font-Manrope sm:text-[14px] text-[12px] font-normal tracking-widest leading-[125%] uppercase relative">
+            Make a reservation
+          </span>
+        </button>
+      </NavLink>
     </div>
     <div className="bg-[#1E2C30] md:w-[50%] w-full sm:relative">
       <div className="bg-bg1 w-full flex lg:py-44 md:py-48 py-36 md:pr-2 pr-4 md:pl-0 pl-4 sm:relative md:justify-start justify-center">

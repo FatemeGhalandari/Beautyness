d231bca1a6f34fc93e736008455faaa6608e73c1
unknown <fghalandarii@gmail.com>
2023-10-24 16:51:21 +0330

    Add Banner component in contact page

---
+++ src/components/Contact/Banner.jsx
@@ -0,0 +1,16 @@
+const Banner = () => {
+  return (
+    <div className="bg-[#1E2C30] w-full sm:relative">
+      <div className="bg-bg1 w-full flex sm:py-20 py-24 sm:px-0 px-6 justify-center flex-col items-center">
+        <p className="text-[#FFF] text-center font-manrope text-[15px] font-normal leading-[154.5%] tracking-wide uppercase">
+          GET IN TOUCH WITH US
+        </p>
+        <h4 className="text-[#FFF] text-center font-playfair sm:text-[50px] text-[40px] font-semibold leading-[120%] tracking-tight max-w-[650px]">
+          We Are Ready To <br/> Assist You 24/7
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;

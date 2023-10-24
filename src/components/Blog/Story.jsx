c6a40c7e0adaf14abea8882b02e5cd7b780a5ae6
unknown <fghalandarii@gmail.com>
2023-10-24 17:55:55 +0330

    Add story component in blog page

---
+++ src/components/Blog/Story.jsx
@@ -0,0 +1,18 @@
+import StoryCard from "./StoryCard";
+import styles from "../../styles";
+import { NavLink } from "react-router-dom";
+const Story = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-20 px-20">
+      <StoryCard />
+      <NavLink to="/Blog">
+        <button type="button" className={`${styles.bt}`}>
+          <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
+          <span className={`${styles.btSpan}`}>VIEW MORE STORIES</span>
+        </button>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Story;

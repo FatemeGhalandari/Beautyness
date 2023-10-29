c3ed3efc7527e298638839071069bf9de5e345bb
unknown <fghalandarii@gmail.com>
2023-10-29 18:44:24 +0330

    Fix bugs

--- src/components/Home/BenefitCard.jsx
+++ src/components/Home/BenefitCard.jsx
@@ -2,11 +2,12 @@ import { tick } from "../../assets";
 import styles from "../../styles";
 
 const BenefitCard = ({ title, content }) => {
+  BenefitCard.propTypes = String;
   return (
     <div className="flex flex-col px-4 py-8 mx-12 max-w-[330px] hover:bg-[#EDF5F7] rounded-[20px] cursor-">
       <img src={tick} alt="tick" className="w-[90px] h-[90px]" />
       <h2 className={`${styles.heading2}`}>{title}</h2>
-      <p className={`${styles.paragraph}` }>{content}</p>
+      <p className={`${styles.paragraph}`}>{content}</p>
     </div>
   );
 };

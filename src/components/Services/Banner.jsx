36eb652657c31927a4a283d785c40858d24d8d40
FatemeGhalandari <fghalandarii@gmail.com>
2024-03-01 19:34:32 +0330

    Fix animation of button

--- src/components/Services/Banner.jsx
+++ src/components/Services/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <NavLink to="/Reservation">
           <button
             type="button"
-            className="flex justify-center items-start py-4 sm:px-8 px-4 bg-[#7A9CA5] sm:mt-8 mt-4 rounded-[4px] group overflow-hidden "
+            className="flex justify-center items-start py-4 sm:px-8 px-4 bg-[#7A9CA5] sm:mt-8 mt-4 rounded-[4px] group overflow-hidden relative"
           >
             <div className="absolute inset-0 w-0 bg-[#5C8692] transition-all duration-[200ms] ease-out group-hover:w-full"></div>
             <span className="text-[#FFF] text-center font-Manrope sm:text-[14px] text-[12px] font-normal tracking-widest leading-[125%] uppercase relative">
